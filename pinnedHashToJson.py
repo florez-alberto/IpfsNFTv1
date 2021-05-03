65fd8be420d4b253bcafb52d2c5a1a8efca5e199
Alberto Florez <florez_alberto@hotmail.com>
2021-05-04 00:48:04 +0200

    messed up json files

--- pinnedHashToJson.py
+++ pinnedHashToJson.py
@@ -15,31 +15,39 @@ descriptions=["The first ones","Algorithmic shuffling with fractals","First Farm
 #author="0"
    
 for i in range(0,72):
-        print (i)
+        print (i+1)
         if i<4 :
             author=author_list[0]
             description=descriptions[0]
+            name=str(i+1)
         elif 4<=i<12 :
             author=author_list[0]
             description=descriptions[1]
+            name=str(i+1)
         elif 12<=i<24:
             author=author_list[0]
             description=descriptions[2]
+            name=str(i+1)
         elif 24<=i<48 :
             author=author_list[2]
             description=descriptions[3]
+            name=data[i]["Name"][:-4]
         elif 48<=i<60 :
            author=author_list[0] 
            description=descriptions[4]
+           name=data[i]["Name"][:-4]
         elif 60<=i<68 :
             author=author_list[7]
             description=descriptions[5] 
+            name=data[i]["Name"][:-4]
         elif 60<=i<72 :
             author=author_list[8]
             description=descriptions[6]
+            name=data[i]["Name"][:-4]
         print(author)
-        # txt='{ "description": "'+description+'", "external_url": "https://gateway.pinata.cloud/ipfs/'+data[i]["Hash"]+'", "image": "https://ipfs.io/ipfs/'+data[i]["Hash"]+'", "name": "'+data[i]["Name"]+'", "author": "'+author+'" }'
-        # file_name =data[i]["Name"]+'.json'
-        # f = open('json/'+file_name, "w+")
-        # f.write(txt)
-        # f.close()
+        print(description)
+        txt='{ "description": "'+description+'", "external_url": "https://gateway.pinata.cloud/ipfs/'+data[i]["Hash"]+'", "image": "https://ipfs.io/ipfs/'+data[i]["Hash"]+'", "name": "'+name+'", "author": "'+author+'" }'
+        #file_name =data[i]["Name"]+'.json'
+        f = open('json/'+str(i+1)+'.json', "w+")
+        f.write(txt)
+        f.close()
